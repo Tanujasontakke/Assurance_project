706565b7b7c3b087d348b38b00f8e42f94ad41c8
Tanujasontakke <40801524+Tanujasontakke@users.noreply.github.com>
2024-04-30 22:11:51 -0700

    updated file

--- test_harness.cpp
+++ test_harness.cpp
@@ -1,47 +1,119 @@
 #include <deepstate/DeepState.hpp>
-#include "your_code_file.hpp" // Replace "your_code_file.hpp" with the actual filename of your code
+#include "CircularDoublyLinkedList.h"
 
 using namespace deepstate;
 
-TEST(CircularDoublyLinkedList, CreateList) {
-    circulardoublylist cdll;
-    // Call the create_node_success function
-    cdll.create_node_success();
+TEST(CircularDoublyLinkedList, InsertAtBeginning) {
+    CircularDoublyLinkedList list;
+    unsigned int num_elements = DeepState_UIntInRange(1, 100);
+
+    for (unsigned int i = 0; i < num_elements; ++i) {
+        int data = DeepState_Int();
+        list.insertAtBeginning(data);
+    }
+
+    ASSERT_EQ(list.size(), num_elements);
 }
 
-TEST(CircularDoublyLinkedList, InsertBegin) {
-    circulardoublylist cdll;
-    int value = DeepState_Int(); // Generate a random integer value
-    cdll.insert_begin(value);
+TEST(CircularDoublyLinkedList, InsertAtEnd) {
+    CircularDoublyLinkedList list;
+    unsigned int num_elements = DeepState_UIntInRange(1, 100);
+
+    for (unsigned int i = 0; i < num_elements; ++i) {
+        int data = DeepState_Int();
+        list.insertAtEnd(data);
+    }
+
+    ASSERT_EQ(list.size(), num_elements);
 }
 
-TEST(CircularDoublyLinkedList, InsertEnd) {
-    circulardoublylist cdll;
-    int value = DeepState_Int(); // Generate a random integer value
-    cdll.insert_end(value);
+TEST(CircularDoublyLinkedList, InsertAtPosition) {
+    CircularDoublyLinkedList list;
+    unsigned int num_elements = DeepState_UIntInRange(1, 100);
+
+    for (unsigned int i = 0; i < num_elements; ++i) {
+        int data = DeepState_Int();
+        unsigned int position = DeepState_UIntInRange(1, list.size() + 1);
+        list.insertAtPosition(data, position);
+    }
+
+    ASSERT_EQ(list.size(), num_elements);
 }
 
-TEST(CircularDoublyLinkedList, InsertPosition) {
-    circulardoublylist cdll;
-    int value = DeepState_Int(); // Generate a random integer value
-    int position = DeepState_Int(); // Generate a random position
-    cdll.insert_position(value, position);
+TEST(CircularDoublyLinkedList, DeleteAtBeginning) {
+    CircularDoublyLinkedList list;
+    unsigned int num_elements = DeepState_UIntInRange(1, 100);
+
+    for (unsigned int i = 0; i < num_elements; ++i) {
+        int data = DeepState_Int();
+        list.insertAtEnd(data);
+    }
+
+    for (unsigned int i = 0; i < num_elements; ++i) {
+        list.deleteAtBeginning();
+    }
+
+    ASSERT_EQ(list.size(), 0);
 }
 
-TEST(CircularDoublyLinkedList, DeleteBegin) {
-    circulardoublylist cdll;
-    // Call the delete_begin function
-    cdll.delete_begin();
+TEST(CircularDoublyLinkedList, DeleteAtEnd) {
+    CircularDoublyLinkedList list;
+    unsigned int num_elements = DeepState_UIntInRange(1, 100);
+
+    for (unsigned int i = 0; i < num_elements; ++i) {
+        int data = DeepState_Int();
+        list.insertAtEnd(data);
+    }
+
+    for (unsigned int i = 0; i < num_elements; ++i) {
+        list.deleteAtEnd();
+    }
+
+    ASSERT_EQ(list.size(), 0);
 }
 
-TEST(CircularDoublyLinkedList, DeleteEnd) {
-    circulardoublylist cdll;
-    // Call the delete_end function
-    cdll.delete_end();
+TEST(CircularDoublyLinkedList, DeleteAtPosition) {
+    CircularDoublyLinkedList list;
+    unsigned int num_elements = DeepState_UIntInRange(1, 100);
+
+    for (unsigned int i = 0; i < num_elements; ++i) {
+        int data = DeepState_Int();
+        list.insertAtEnd(data);
+    }
+
+    for (unsigned int i = 0; i < num_elements; ++i) {
+        unsigned int position = DeepState_UIntInRange(1, list.size() + 1);
+        list.deleteAtPosition(position);
+    }
+
+    ASSERT_EQ(list.size(), 0);
 }
 
-TEST(CircularDoublyLinkedList, DeletePosition) {
-    circulardoublylist cdll;
-    int position = DeepState_Int(); // Generate a random position
-    cdll.delete_position(position);
+TEST(CircularDoublyLinkedList, ConcatenateAndSplit) {
+    CircularDoublyLinkedList list1;
+    CircularDoublyLinkedList list2;
+
+    unsigned int num_elements1 = DeepState_UIntInRange(1, 100);
+    unsigned int num_elements2 = DeepState_UIntInRange(1, 100);
+
+    for (unsigned int i = 0; i < num_elements1; ++i) {
+        int data = DeepState_Int();
+        list1.insertAtEnd(data);
+    }
+
+    for (unsigned int i = 0; i < num_elements2; ++i) {
+        int data = DeepState_Int();
+        list2.insertAtEnd(data);
+    }
+
+    list1.concatenate(list2);
+    ASSERT_EQ(list1.size(), num_elements1 + num_elements2);
+    ASSERT_EQ(list2.size(), 0);
+
+    CircularDoublyLinkedList list3;
+    CircularDoublyLinkedList list4;
+
+    list1.split(list3, list4);
+    ASSERT_EQ(list3.size() + list4.size(), num_elements1 + num_elements2);
+    ASSERT_EQ(list1.size(), 0);
 }
